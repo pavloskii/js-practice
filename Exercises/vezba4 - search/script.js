6514b14733864f9984b9d181f4727869acb35997
pavloskii <pavlo.igor@gmail.com>
2017-02-18 04:06:30 +0100

    a
    a

---
+++ Exercises/vezba4 - search/script.js
@@ -0,0 +1,39 @@
+$(() => {
+    $.ajax("gbooks.json", {
+        complete: (data) => {
+            allBooks = data.responseJSON;
+            clearTable();
+            allBooks.forEach(b => displayBook(b));
+        }
+    });
+
+    //  $("#search").on("click", () => {
+    //     let searchTerm = $("#searchTerm").val().toLowerCase();
+    //     let books = allBooks.filter(b => {
+    //         if (b.author.toLowerCase().indexOf(searchTerm) !== -1)
+    //             return true;
+    //         return false;
+    //     });
+    //     clearTable();
+    //     books.forEach(b => displayBook(b)); 
+    // })
+
+
+});
+
+
+let displayBook = (book) => {
+    let bookList = $("#bookList");
+
+    bookList.append(`<tr>
+        <td>${book.author}</td>
+        <td>${book.title}</td>
+        <td>0</td>
+        <td><img src="${book.img}"></td>
+    </tr>`)
+}
+
+let clearTable = () => {
+    let bookList = $("#bookList");
+    bookList.html("");
+}
